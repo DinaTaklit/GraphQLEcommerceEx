3aca7324c9602f5bbc81e3c52df586fb291ae53e
Dina Taklit <ez_taklit@esi.dz>
2023-07-10 10:07:19 +0100

    feat: query products

--- index.js
+++ index.js
@@ -1,17 +1,59 @@
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 
+const products = [
+  {
+    id: "53a0724c-a416-4cac-ae45-bfaedce1f147",
+    name: "Steel Pot",
+    description: "Silver steel pot that is perfect for cooking",
+    image: "imgage1",
+    quantity: 230,
+    price: 42.44,
+    onSale: false,
+  },
+  {
+    id: "c2af9adc-d0b8-4d44-871f-cef66f86f7f6",
+    name: "Salad Bowl",
+    description: "Round wooden bowl perfect for tossing and making salads",
+    quantity: 33,
+    image: "imgage2",
+    price: 53.5,
+    onSale: false,
+  },
+  {
+    id: "2c931e7e-510f-49e5-aed6-d6b44087e5a1",
+    name: "Spoon",
+    description: "Small and delicate spoon",
+    image: "imgage3",
+    quantity: 4266,
+    price: 1.33,
+    onSale: true,
+  },
+];
+
 const typeDefs = `#graphql
+
 type Query{
     helloWorld: String
-    myName: String
+    products: [Product!]!
 }
+
+#Define a product type definition 
+type Product{
+  name: String!
+  description: String!
+  quantity: Int!
+  image: String!
+  price: Float!
+  onSale: Boolean!
+}
+
 `;
 
 const resolvers = {
   Query: {
     helloWorld: () => "Hello World",
-    myName: () => "Dina TAKLIT",
+    products: () => products,
   },
 };
 
