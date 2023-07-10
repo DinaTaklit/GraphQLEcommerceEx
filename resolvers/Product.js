90b80df7aed32203ae0893e31439916818ff725b
Dina Taklit <ez_taklit@esi.dz>
2023-07-10 23:44:08 +0100

    feat: get reviews of each product

--- resolvers/Product.js
+++ resolvers/Product.js
@@ -1,5 +1,7 @@
 const Product = {
   category: ({ categoryId }, args, { categories }) =>
     categories.find((category) => category.id === categoryId),
+  reviews: ({ id: productId }, args, { reviews }) =>
+    reviews.filter((review) => review.productId === productId),
 };
 export default Product;
